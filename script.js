628ced16dc278cd12236f3466c579aaecccbe4b3
Sreechakram <79197163+Sreechakram@users.noreply.github.com>
2021-06-08 20:36:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+setTimeout(()=>{
+    document.write("10<br>");
+    setTimeout(()=>{
+        document.write("9<br>");
+        setTimeout(()=>{
+            document.write("8<br>");
+            setTimeout(()=>{
+                document.write("7<br>");
+                setTimeout(()=>{
+                    document.write("6<br>");
+                    setTimeout(()=>{
+                        document.write("5<br>");
+                        setTimeout(()=>{
+                            document.write("3<br>");
+                            setTimeout(()=>{
+                                document.write("2<br>");
+                                setTimeout(()=>{
+                                    document.write("1<br>");
+                                    setTimeout(()=>{
+                                        document.write("TASK DONE");
+},3000);
+},3000);
+},3000);
+},3000);
+},3000);
+},3000);
+},3000);
+},3000);
+},3000);
+},3000);
